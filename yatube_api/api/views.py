a714f4f77c61c3434645f63e67ccc65d348cbfe9
Давид <dovmont1rambler.ru@yandex.ru>
2021-09-19 22:46:31 +0300

    вариант 2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,8 @@
 from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
-                                        PermissionDenied)
+                                        IsAuthenticated)
 from rest_framework.exceptions import PermissionDenied
 from django_filters.rest_framework import DjangoFilterBackend
 
